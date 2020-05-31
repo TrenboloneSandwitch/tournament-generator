7568906fda4aeb057789b79641a6607e0f3b6bca
Bobby Sixkiller <soldier068@email.cz>
2020-05-31 22:46:09 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import CreateTournament from "./components/CreateTournament";
 
 function App() {
+  const [pageBg, setPageBg] = useState("trophy.jpg");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(./assets/${pageBg})` }}>
+      <CreateTournament />
     </div>
   );
 }
---
+++ src/components/CreateTournament.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateTournament = () => {
+  return (
+    <div>
+      <h2>zdarek</h2>
+    </div>
+  );
+};
+
+export default CreateTournament;
